fb986414aef4ac6091180db6fb3f7b6980b7de2d
HenrikH <henrik.hellem@live.com>
2023-08-09 20:34:07 +0200

    initial commit

---
+++ game.js
@@ -0,0 +1,17 @@
+const options = {
+    0: "Rock",
+    1: "Paper",
+    2: "Scissor"
+}
+
+
+const getComputerChoice = () => options[Math.floor(Math.random() * 3)];
+
+function playRound(playerSelection, computerSelection){
+    
+}
+
+
+
+
+console.log(getComputerChoice())

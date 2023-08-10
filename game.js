e2d39123cc809c8f5d745065a18bd01c001fe91a
HenrikH <henrik.hellem@live.com>
2023-08-10 19:49:33 +0200

    playround fundtion case insensitive

--- game.js
+++ game.js
@@ -1,17 +1,26 @@
 const options = {
-    0: "Rock",
-    1: "Paper",
-    2: "Scissor"
+    0: "rock",
+    1: "paper",
+    2: "scissor"
 }
 
 
 const getComputerChoice = () => options[Math.floor(Math.random() * 3)];
 
-function playRound(playerSelection, computerSelection){
-    
+function playRound(playerSelectionRaw, computerSelection){
+    let playerSelection = playerSelectionRaw.toLowerCase() // make case insensitive
+    if (  playerSelection === computerSelection){
+        return "Tie!"
+    }
+    if( (playerSelection === "rock" && computerSelection === "scissor") || (playerSelection === "scissor" ))
+        
+    }
+
 }
 
 
 
 
 console.log(getComputerChoice())
+
+console.log(playRound("Rock", getComputerChoice()))

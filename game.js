cb47b82190ba25fa5be4308b99303b4a52ae5f64
HenrikH <henrik.hellem@live.com>
2023-08-11 09:15:59 +0200

    Playround ok

--- game.js
+++ game.js
@@ -1,26 +1,38 @@
 const options = {
-    0: "rock",
-    1: "paper",
-    2: "scissor"
+  0: "rock",
+  1: "paper",
+  2: "scissor",
+};
+
+let scoreObj = {
+    playerScore : 0,
+    computerScore : 0,
 }
 
 
 const getComputerChoice = () => options[Math.floor(Math.random() * 3)];
 
-function playRound(playerSelectionRaw, computerSelection){
-    let playerSelection = playerSelectionRaw.toLowerCase() // make case insensitive
-    if (  playerSelection === computerSelection){
-        return "Tie!"
-    }
-    if( (playerSelection === "rock" && computerSelection === "scissor") || (playerSelection === "scissor" ))
-        
-    }
+function playRound(playerSelectionRaw, computerSelection) {
+  let playerSelection = playerSelectionRaw.toLowerCase(); // make case insensitive, could perhaps use locale.compare
 
+  if (!Object.values(options).includes(playerSelection)) return "FAIL!";
+  if (playerSelection === computerSelection) return "Tie!";
+
+  return (playerSelection === "rock" && computerSelection === "scissor") ||
+    (playerSelection === "scissor" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+    ? "You win! " + playerSelection + " beats " // could have used template literals
+    : "You loose! " + computerSelection + " beats " + playerSelection;
 }
 
 
+function game(){
 
+    while(playerScore <= 5 || computerScore <= 5){
+        playRound(prompt("Choose", getComputerChoice))
+    }
+}
 
-console.log(getComputerChoice())
+console.log(getComputerChoice());
 
-console.log(playRound("Rock", getComputerChoice()))
+console.log(playRound("rock", getComputerChoice()));

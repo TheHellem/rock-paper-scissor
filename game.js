f0b2f84c9c3ad993e406457bfe41c49120e936c9
HenrikH <henrik.hellem@live.com>
2023-08-15 18:39:21 +0200

    Update playRound function (not working atm)

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+const playerChoice = document.querySelectorAll(".options");
+const computerScoreDisplay = document.querySelector("#computer-score");
+
 const options = {
   0: "rock",
   1: "paper",
@@ -8,24 +11,45 @@ let scoreObj = {
   playerScore: 0,
   computerScore: 0,
 };
+
 const getComputerChoice = () => options[Math.floor(Math.random() * 3)];
 
-function playRound(playerSelectionRaw, computerSelection) {
-  let playerSelection = playerSelectionRaw.toLowerCase(); // make case insensitive, could perhaps use locale.compare
-  console.log(`computerSelection ${computerSelection}`);
-  console.log(`playerSelection ${playerSelection}`);
+function playRound(playerSelection, computerSelection) {
+  // Er map en dårlig ide, hjelpefunksjon i stedet? Skal ikke printe noe på skjerm.
+  const resultMap = new Map();
+
+  console.log("player: " + playerSelection);
+  console.log("computer: " + computerSelection);
+
+  if (playerSelection === computerSelection)
+    resultMap.set("tie", `It is a tie! You both chose ${playerSelection}!`);
+  else {
+    (playerSelection === "rock" && computerSelection === "scissor") ||
+    (playerSelection === "scissor" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+      ? resultMap.set("winner", `You won this round!  `)
+      : (scoreObj.computerScore += 1);
+  }
+  return resultMap;
+}
+
+const getPlayerChoice = (e) => {
+  console.log(playRound(e.target.id, getComputerChoice()));
+};
+playerChoice.forEach((item) => {
+  item.addEventListener("click", getPlayerChoice);
+});
+
+/*
 
 
-  if (!Object.values(options).includes(playerSelection)) return "FAIL!";
-  if (playerSelection === computerSelection) return "Tie!";
-
-  return (playerSelection === "rock" && computerSelection === "scissor") ||
+  (playerSelection === "rock" && computerSelection === "scissor") ||
     (playerSelection === "scissor" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
     ? (scoreObj.playerScore += 1)
     : (scoreObj.computerScore += 1);
 }
-/*
+
 function game() {
   while (scoreObj.playerScore <= 5 && scoreObj.computerScore <= 5) {
     playRound(prompt("Choose"), getComputerChoice());
@@ -36,4 +60,4 @@ function game() {
 }
 */
 //console.log(getComputerChoice());
-console.log(game());
+console.log(playerChoice);
